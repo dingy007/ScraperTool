23f97ba985fe1741e924168fa273d9f7f74b771c
dingy007 <dingy007@gmail.com>
2015-12-11 17:34:57 +0800

    Updating the HTML parsing. WIP

--- src/main/java/com/scraper/tool/ParseHTML.java
+++ src/main/java/com/scraper/tool/ParseHTML.java
@@ -40,29 +40,46 @@ public class ParseHTML {
 	 * @return mappedData Map of all the required Headers vs Data
 	 */
 	public static Map<String, String> extractReqdDataFrmWikiPg (Document doc) {
-		Map<String, String> mappedData = new HashMap<String, String>();
-
+		Map<String, String> layoutData = new HashMap<String, String>();
+		Integer count =0;
 		System.out.println("*********************");
 		System.out.println("Extracting the required data from page: ParseHTML.extractReqdDataFrmWikiPg()");
 		if (doc.title().isEmpty()) {
 			throw new RuntimeException("The HTML is invalid.");
 		}
 		System.out.println("Document title is: "+doc.title());
+		//		Elements tableElement = doc.getElementsByClass("yfi_quote_summary").select("tr"); //This is the Content that we are interested in.
+		Element layout = doc.getElementsByAttributeValue("Id", "Layout").first().parent();
+		Element afterLayout = layout.nextElementSibling();
+		System.out.println(afterLayout);
+
 		
-//		Elements tableElement = doc.getElementsByClass("yfi_quote_summary").select("tr"); //This is the Content that we are interested in.
-		Elements tables = doc.select("Table");
-//		System.out.println(tables.toString());
+		//		do {
+//			tr = afterLayout.select()
+//		}
+		//		System.out.println(tables.toString());
+		/*
 		if (tables.isEmpty()) {
 			throw new RuntimeException("The extract table was not found. Please check the HTML page.");
 		}
-		for (Element eachTableElement : tables) { //stepping through each element of the table
-						System.out.println("Table Header: \n" + eachTableElement.select("th").text());  //Enable for DEBUG, this prints the table headers
-//			mappedData.put(eachTableElement.select("th").text(), eachTableElement.select("td").text());
-						System.out.println("\n\t" + eachTableElement.select("td").text()); //Enable for DEBUG, this prints the table data
-		}
+		for (Element tableElement : tables) { //stepping through each element of the table
+			System.out.println(tables.text());
+			System.out.println("Table Header: \n" + tableElement.select("th").text());  //Enable for DEBUG, this prints the table headers
+			if (tableElement.hasAttr("rowspan")) {
+				count =Integer.parseInt(tableElement.getElementsByAttribute("rowspan").toString());
+				String lvl = tableElement.select("th").text();
+				System.out.println("Level: " + lvl);
+				Elements tRows = tableElement.getElementsByTag("tr");
+				for (Element tRow : tRows) {
+					System.out.println(tRow.text());
+				}
+			}*/
+		//			mappedData.put(eachTableElement.select("th").text(), eachTableElement.select("td").text());
+		//System.out.println("\n\t" + tableElement.select("td").text()); //Enable for DEBUG, this prints the table data
+		//		}
 		System.out.println("*********************");
 
-		return mappedData;
+		return layoutData;
 
 	}
 
